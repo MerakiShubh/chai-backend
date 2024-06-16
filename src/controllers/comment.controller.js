e195543e7f03e66657ac152fe639dbc6c8bd6379
MerakiShubh <shubhamt.champ@gmail.com>
2024-06-17 00:46:21 +0530

    Delete comment of a video

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,7 +8,7 @@ import { Video } from "../models/video.model.js";
 const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
-  const { page, limit } = req.query;
+  const { page = 1, limit = 10 } = req.query;
 
   if (!videoId || !mongoose.Types.ObjectId.isValid(videoId)) {
     throw new ApiError(400, "Valid video id is required");
@@ -123,6 +123,16 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+  const { commentId } = req.params;
+  if (!commentId) throw new ApiError(400, "Comment id is required");
+  const comment = await Comment.findById(commentId);
+  if (!comment) throw new ApiError(404, "Comment does not exist");
+  if (comment.owner.toString() !== req.user._id.toString())
+    throw new ApiError(404, "Not authorized to delete this comment");
+  await Comment.findByIdAndDelete(commentId);
+  res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Comment deleted successfully"));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
